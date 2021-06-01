4ab73219316c88f840aac8d4e269ca6432f3aa86
Steven Wilkin <stevenwilkin@gmail.com>
2021-06-01 23:52:13 +0700

    Add empty activity

---
+++ app/src/main/java/com/nulltheory/reps/RepsActivity.kt
@@ -0,0 +1,11 @@
+package com.nulltheory.reps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RepsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reps)
+    }
+}
