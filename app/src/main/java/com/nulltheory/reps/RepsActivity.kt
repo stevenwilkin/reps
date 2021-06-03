b5cff7e8983ded9363aba19ace0cbb3ac25d3e07
Steven Wilkin <stevenwilkin@gmail.com>
2021-06-03 19:59:22 +0700

    Increment and reset rep count

--- app/src/main/java/com/nulltheory/reps/RepsActivity.kt
+++ app/src/main/java/com/nulltheory/reps/RepsActivity.kt
@@ -2,10 +2,31 @@ package com.nulltheory.reps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+lateinit var textRep: TextView
+var reps: Int = 0
 
 class RepsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reps)
+        textRep = findViewById(R.id.text_rep)
+        displayReps()
+    }
+
+    private fun displayReps() {
+        textRep.text = reps.toString()
+    }
+
+    fun increaseReps(view: View) {
+        reps++
+        displayReps()
+    }
+
+    fun resetReps(view: View) {
+        reps = 0
+        displayReps()
     }
 }
