90a9f8c310f7ce5fc5e53675116f863ebec77500
Steven Wilkin <stevenwilkin@gmail.com>
2021-09-10 21:51:52 +0700

    Handle all UI updates in a single place

--- app/src/main/java/com/nulltheory/reps/RepsActivity.kt
+++ app/src/main/java/com/nulltheory/reps/RepsActivity.kt
@@ -9,78 +9,71 @@ import android.widget.Button
 import android.widget.TextView
 import java.text.SimpleDateFormat
 
-lateinit var textRep: TextView
-lateinit var textTime: TextView
-lateinit var buttonTime: Button
-var reps: Int = 0
-var startTime: Long = 0
-var elapsedTime: Long = 0
-
 class RepsActivity : AppCompatActivity() {
+    lateinit var mainHandler: Handler
+    lateinit var textRep: TextView
+    lateinit var textTime: TextView
+    lateinit var buttonTime: Button
+    var reps: Int = 0
+    var startTime: Long = 0
+    var elapsedTime: Long = 0
+
+    private val updateDisplayTask = object : Runnable {
+        override fun run() {
+            var elapsed = elapsedTime
+
+            if(startTime == 0L) {
+                buttonTime.text = getString(R.string.start)
+            } else {
+                elapsed += (System.currentTimeMillis() - startTime)
+                buttonTime.text = getString(R.string.stop)
+            }
+
+            textTime.text = SimpleDateFormat("mm:ss").format(elapsed)
+            textRep.text = reps.toString()
+
+            mainHandler.postDelayed(this, 100)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reps)
         textRep = findViewById(R.id.text_rep)
         textTime = findViewById(R.id.text_time)
         buttonTime = findViewById(R.id.button_time)
-        displayReps()
-
-        val mainHandler = Handler(Looper.getMainLooper())
-        mainHandler.post(object: Runnable {
-            override fun run() {
-                displayTime()
-                mainHandler.postDelayed(this, 100)
-            }
-        })
+        mainHandler = Handler(Looper.getMainLooper())
     }
 
-    private fun displayReps() {
-        textRep.text = reps.toString()
+    override fun onResume() {
+        super.onResume()
+        mainHandler.post(updateDisplayTask)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mainHandler.removeCallbacks(updateDisplayTask)
     }
 
     fun increaseReps(view: View) {
         reps++
-        displayReps()
     }
 
     fun resetReps(view: View) {
         reps = 0
-        displayReps()
     }
 
-    private fun displayTime() {
+    fun toggleTimer(view: View) {
         if(startTime == 0L) {
-            return
-        }
-
-        val elapsed = System.currentTimeMillis() - startTime + elapsedTime
-        textTime.text = SimpleDateFormat("mm:ss").format(elapsed)
-    }
-
-    fun startTimer(view: View) {
-        startTime = System.currentTimeMillis()
-        buttonTime.text = getString(R.string.stop)
-        buttonTime.setOnClickListener {
-            stopTimer(it)
-        }
-    }
-
-    fun stopTimer(view: View) {
-        elapsedTime += (System.currentTimeMillis() - startTime)
-        startTime = 0
-        buttonTime.text = getString(R.string.start)
-        buttonTime.setOnClickListener {
-            startTimer(it)
+            startTime = System.currentTimeMillis()
+        } else {
+            elapsedTime += (System.currentTimeMillis() - startTime)
+            startTime = 0
         }
     }
 
     fun resetTimer(view: View) {
         startTime = 0
         elapsedTime = 0
-        textTime.text = getString(R.string.zero_time)
-        buttonTime.text = getString(R.string.start)
-        buttonTime.setOnClickListener {
-            startTimer(it)
-        }
     }
 }
